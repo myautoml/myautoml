32f21f10874cf0a523f4bdfae6980f5fc87a39d3
Erik Jan de Vries <erikjandevries@users.noreply.github.com>
2020-10-18 17:29:59 +0200

    Fixed import

--- src/myautoml/__init__.py
+++ src/myautoml/__init__.py
@@ -1,7 +1,7 @@
 from .__about__ import (
-    __title__, __summary__, __uri__, __version__, __author__, __email__, __license__, __copyright__,
+    __title__, __summary__, __uri__, __version__, __author__, __license__, __copyright__,
 )
 
 __all__ = [
-    "__title__", "__summary__", "__uri__", "__version__", "__author__", "__email__", "__license__", "__copyright__",
+    "__title__", "__summary__", "__uri__", "__version__", "__author__", "__license__", "__copyright__",
 ]
